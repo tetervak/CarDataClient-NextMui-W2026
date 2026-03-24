6b9db08999633cc3727e95cf51e07255df660a6d
Alex Tetervak <tetervak@sheridancollege.ca>
2026-03-24 09:11:34 -0400

    added MUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+      weight: ['300', '400', '500', '700'],
+      subsets: ['latin'],
+      display: 'swap',
+      variable: '--font-roboto',
+    });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
-    </html>
+      <html lang="en" className={roboto.variable}>
+      <body>
+      <AppRouterCacheProvider>
+        <ThemeProvider theme={theme}>
+        {children}
+        </ThemeProvider>
+      </AppRouterCacheProvider>
+      </body>
+      </html>
   );
 }
